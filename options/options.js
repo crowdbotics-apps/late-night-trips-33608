20f50a1393fc41e9ddbf6588303a8727d791c518
Crowdbotics <team@crowdbotics.com>
2022-02-14 19:39:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "late_night_trips_33608",
+  url: "https://late_night_trips_33608.botics.co",
+  api: "https://late_night_trips_33608.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
